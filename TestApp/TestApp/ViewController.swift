6eae67e1e8957ac77e361f6ed21620678f058154
LeviAckerman <aandreydoroshev1995@mail.ru>
2025-02-21 12:00:22 +0300

    create app and delete storyboard

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Andrey on 21.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
