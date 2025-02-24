0694887227b7cb9837c1c785f6cd6ce4a7fd7f8a
LeviAckerman <aandreydoroshev1995@mail.ru>
2025-02-24 10:24:53 +0300

    added roboto font

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
+        view.backgroundColor = .brown
+        
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 50)
+        ])
+        //label.font = .Roboto.thim.size(20)
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 40,weight: UIFont.Weight(rawValue: 20))
     }
 
 
